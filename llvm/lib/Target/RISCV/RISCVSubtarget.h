58c6c2b3f41286b5ead26d98c2b9dc1a96ffc05e
Jovan Dmitrovic <Jovan.Dmitrovic@Syrmia.com>
2023-03-16 15:52:30 +0100

    RISCV: EXT/INS instructions added

--- llvm/lib/Target/RISCV/RISCVSubtarget.h
+++ llvm/lib/Target/RISCV/RISCVSubtarget.h
@@ -130,6 +130,8 @@ public:
   }
   bool is64Bit() const { return IsRV64; }
   bool shouldRemoveBackToBackBranches() const;
+  bool shouldUseExt() const;
+  bool shouldUseIns() const;
   bool useLoadStorePairs() const;
   bool useCCMovInsn() const;
   MVT getXLenVT() const { return XLenVT; }
