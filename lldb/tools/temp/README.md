9398a5bf45ef882005d3c30f294e1f08d790b038
Bogdan Zunic <Bogdan.Zunic@Syrmia.com>
2023-07-24 15:10:26 +0200

    Updated the the temp tool and README.md

--- lldb/tools/temp/README.md
+++ lldb/tools/temp/README.md
@@ -1,21 +1,41 @@
 # Basic Debug Tool
 ## Description:
-Runs the provided executable and prints the line number of the first line in the main method
+Runs a basic wrapper for lldb. Sets the provided executable as the target, sets a breakpoint on the main function and starts debugging.
 ## Build:
   * cmake <other_build_options> -DLLVM_ENABLE_PROJECTS="clang;lldb" <other_build_options>
   * ninja lldb
+  * If lldb is already built: ninja temp
     
   For more info about other build options read: https://llvm.org/docs/GettingStarted.html#checkout-llvm-from-git
 ## Run:
  $ temp <executable_path>
 
 ## Output:
-   * Prints the line number first line of main method (if it exists) when reached 
+   * Starts debugging the provided executable
+   * Sets a breakpoint on the 'main' function
    * Prints any output that the executable prints
 ## Example
     $ temp ./basic.out
     INPUT: ./basic.out
-    Process 64131 running
-    Cur Line:11
-    Process 64131 running
+    r
+    Process 49371 launched: '<path>/test.out' (x86_64)
+    Process 49371 running
+    Process 49371 stopped
+    thread #1: tid = 49371, 0x00005555555551df test.out`main at basic.cpp:11:7, name = 'test.out', stop reason = breakpoint 1.1
+    frame #0: 0x00005555555551df test.out`main at basic.cpp:11:7
+       8   	
+       9   	int main(){
+       10  	
+    -> 11  	  int t  = 5;
+       12  	  ding(&t);
+       13  	
+       14  	   std::cout<<"ding"<<std::endl;
+    c
+    Process 49371 resuming
+    Process 49371 running
+    ding
+    Process 49371 exited with status = 0 (0x00000000) 
+
+
     
+
--- lldb/tools/temp/TestTools.cpp
+++ lldb/tools/temp/TestTools.cpp
@@ -13,6 +13,11 @@ using namespace std;
 #include "lldb/API/SBFile.h" 
 #include "lldb/API/SBFrame.h"
 #include "lldb/API/SBThread.h"
+#include "lldb/API/SBStream.h"
+#include "lldb/API/SBSourceManager.h"
+#include "lldb/API/SBExecutionContext.h"
+#include "lldb/API/SBCommandReturnObject.h"
+#include "lldb/API/SBCommandInterpreter.h"
 
 using namespace llvm;
 
@@ -32,6 +37,8 @@ static opt<std::string> TargetFileName("target-file", cl::Positional,
 
 
 int main(int argc, char **argv) {
+
+
   cl::ParseCommandLineOptions(argc, argv, "temp\n");
 
   llvm::outs() << "INPUT: " << TargetFileName.c_str()<<'\n';
@@ -44,39 +51,155 @@ int main(int argc, char **argv) {
   debugger.SetOutputFileHandle(stdout,false);
   debugger.SetInputFileHandle(stdin,true);
 
-
-  std::string s1 = TargetFileName.c_str();
-  std::string s = Twine("file " + s1).str();
-
+  //Create a target from the provided exe file
+  //Exit if the target isn't created
   lldb::SBTarget target = debugger.CreateTarget(TargetFileName.c_str());
+  if(!target.IsValid()){
+    return -1;
+  }
+
+  //Setup
+  //Set target and a breakpoint for the main function(if it exists)
   debugger.SetSelectedTarget(target);
-  lldb::SBLaunchInfo li = target.GetLaunchInfo();
 
-  lldb::SBError err;
-
-  lldb::SBListener lis = debugger.GetListener();
-
-  li.SetListener(lis);
   target.BreakpointCreateByName("main");
-  target.SetLaunchInfo(li);
 
-  lldb::SBProcess proc= target.Launch(li,err);
-
-  lldb::SBEvent event;
+  //Initial loop
+  //We run this loop until the user quits or starts the process from the proces
+  while (true)
+  {
+    string buff;
+        getline(cin,buff);
+        debugger.HandleCommand(buff.c_str());
+        
+        if(debugger.GetCommandInterpreter().HasCustomQuitExitCode()){
+          return 0;
+        }
+        else if(buff=="q"){
+          return 0;
+        }
+        else if(target.GetProcess().IsValid()){
+          break;
+        }
+  }
   
-  lldb::SBError err2;
-  while(proc.GetExitStatus()==-1){
+  //Setup support vars for main loop
+  lldb::SBProcess proc = target.GetProcess();
+  lldb::SBEvent event;
+  lldb::SBStream str_temp;
+  lldb::SBListener lis = debugger.GetListener();
+  str_temp.RedirectToFileHandle(stdout,false);
+
+  //Main loop
+  //We run this loop untill the running process exits
+  //The process can be killed and replaced by the user but the loop terminates only when it exits
+  while(true){
+    //Get current process
+    //Required since the user can use commands such as 'r' durring debugging to kill and start a new process
+    proc = target.GetProcess();
+
+    //Handle events
     while (lis.GetNextEvent(event))
     {
-      // cout<<event.GetType()<<endl;
       debugger.HandleProcessEvent(proc,event,debugger.GetOutputFile(),debugger.GetErrorFile());
     }
+    //Breakpoint hit
     if (proc.GetState()==lldb::eStateStopped){
-      lldb::SBAddress addr = proc.GetThreadAtIndex(0).GetSelectedFrame().GetPCAddress();
-      lldb::SBSymbolContext sy_cx = proc.GetTarget().ResolveSymbolContextForAddress(addr,lldb::eSymbolContextEverything);
-      cout<<"Cur Line:"<<sy_cx.GetLineEntry().GetLine()<<endl;
-      proc.Continue();
-    }
+      
+        str_temp.Printf("Process %" PRIu64" stopped\n",proc.GetProcessID());
+
+        //Get the current thread and selected frame
+        lldb::SBThread cur_thread = proc.GetThreadAtIndex(0);
+        lldb::SBFrame selected_frame = cur_thread.GetSelectedFrame();
+        
+        //Print the current thread and selected frame info
+        cur_thread.GetDescription(str_temp);
+        selected_frame.GetDescription(str_temp);
+        
+        //Get the SymbolContext for source line printing
+        lldb::SBAddress addr = selected_frame.GetPCAddress();
+        lldb::SBSymbolContext sy_cx = target.ResolveSymbolContextForAddress(addr,lldb::eSymbolContextEverything);
+        
+        //Are we stil in the source file ?
+        if(selected_frame.GetLineEntry().IsValid()){
+          //Print 3 lines of source code before and after the current line
+          target.GetSourceManager().DisplaySourceLinesWithLineNumbersAndColumn(sy_cx.GetLineEntry().GetFileSpec(),sy_cx.GetLineEntry().GetLine(),sy_cx.GetLineEntry().GetColumn(),3,3,"->",str_temp);
+        }
+        else{
+          //Switched to ASM
+
+          //Print asm function
+          //Print format similar to LLDB
+          cout<<selected_frame.GetModule().GetFileSpec().GetFilename()<<"`"<<selected_frame.GetFunctionName()<<':'<<endl;
+          
+          //Offset in the function
+          // cout<<" + "<<selected_frame.GetPCAddress().GetOffset()-selected_frame.GetModule().ResolveSymbolContextForAddress(selected_frame.GetPCAddress(),lldb::eSymbolContextEverything).GetSymbol().GetStartAddress().GetOffset();
+
+          //Get asm
+          string c = selected_frame.Disassemble();
+          
+          //Find the current inst
+          size_t idx = c.find("->",0);
+          //Include color
+          idx -=5;
+
+          //Print next 4 lines
+          if(idx!=string::npos){
+            size_t lines = 4;
+            while (lines>0 && idx<c.size())
+            {
+              cout<<c.c_str()[idx];
+              if(c[idx]=='\n'){
+                lines--;
+              }
+              idx++;
+            }
+            cout<<endl;
+          }
+        
+        }
+        //Handle new command
+        string buff;
+        getline(cin,buff);
+        
+        //Special case 
+        //If the input is the quit command we need to make sure the user acctualy quits
+        //When the command is executed the user will be asked if the process should be killed
+        //If the user chooses not to kill the process the loop continues
+        if(buff=="q"){
+          lldb::SBCommandReturnObject result;
+          //Manualy handle the command via the SBCommandInterpeter in order to get the result
+          debugger.GetCommandInterpreter().HandleCommand(buff.c_str(),result);
+          
+
+          //Did the user choose to kill the process ?
+          if(result.GetStatus() & lldb::eReturnStatusQuit){
+            break;
+          }
+        }
+        else{
+          debugger.HandleCommand(buff.c_str());
+        }
+        
+        //Check if we have quit with or without and exitcode
+        if(debugger.GetCommandInterpreter().GetQuitStatus()){
+          break;
+        }
+      }
+      else if(proc.GetState()==lldb::eStateExited){
+        //If the process has exited print its status
+        int exit_status = proc.GetExitStatus();
+        const char* exit_desc = proc.GetExitDescription();
+        printf("Process %" PRIu64 " exited with status = %i (0x%8.8x) %s\n",
+                    proc.GetProcessID(), exit_status, exit_status,
+                    exit_desc ? exit_desc : "");
+        break;
+        
+      }
   }
+    //If the user has quit, kill the process
+    if(proc.IsValid()){
+      proc.Kill();
+    }
     return 0;
 }
